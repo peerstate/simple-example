45ecc96bd04fecbe7676c128802fe0f54f58486f
Bryan Goldstein <brysgo@gmail.com>
2020-04-04 20:56:18 -0400

    switch to basic boilerplate and use @peerstate/core

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { Operation, compare } from "fast-json-patch";
-import React, { useEffect } from "react";
+import React, { useEffect, useReducer, Reducer } from "react";
 import Gun from "gun/gun";
 
-import Messenger from "../Messenger";
 import {
   createAuthFilter,
   createEncryptionFilter,
-  usePeerState,
   createKeychain,
-  Action
-} from "../../peerstate";
-import { MatchResult } from "path-to-regexp";
+  createPeerState,
+  Action,
+  Keychain,
+} from "@peerstate/core";
 
+// TODO: use @peerstate/react instead of DIY
+// import { usePeerState } from "@peerstate/react";
+
+type InternalState<T> = {
+  peerState: T;
+  keys: Keychain;
+};
 type StateTreeType = any;
 
 const myAuthFilter = createAuthFilter<StateTreeType>({
   "/lastName": () => true,
-  "/users/:userId": (
-    senderId: string,
-    state: StateTreeType,
-    action: Operation,
-    params: MatchResult
-  ) => true,
-  "/counter": () => true
+  "/users/:userId": () => true,
+  "/counter": () => true,
 });
 
 const myEncryptionFilter = createEncryptionFilter<StateTreeType>({
   "/lastName": () => ["2"],
-  "/counter": () => ["2"]
+  "/counter": () => ["2"],
 });
 
 // TODO: put this URL in the right place
@@ -37,15 +37,28 @@ const keychain = createKeychain(SERVER_URL);
 let userId: string = keychain.getUserInfo().id;
 
 // TODO: put this URL in the right place
-const gun = Gun(["https://gunjs.herokuapp.com/gun"]);
+const gun = Gun(["https://gun-matrix.herokuapp.com/gun"]);
 
 export default function App() {
-  const { state, dispatch, sign } = usePeerState<StateTreeType>(
-    {},
+  const { nextState, sign: signWithState } = createPeerState(
     myAuthFilter,
     myEncryptionFilter,
     keychain
   );
+  const [internalState, dispatch] = useReducer<
+    Reducer<InternalState<StateTreeType>, Action>
+  >(nextState, {
+    peerState: {},
+    keys: keychain,
+  });
+  const state = internalState.peerState;
+  const sign = signWithState.bind(null, state);
+  // const { state, dispatch, sign } = usePeerState<StateTreeType>(
+  //   {},
+  //   myAuthFilter,
+  //   myEncryptionFilter,
+  //   keychain
+  // );
 
   useEffect(() => {
     return gun
@@ -53,7 +66,7 @@ export default function App() {
       .get(userId)
       .map()
       .on((action: any) => dispatch(action as Action)).off;
-  }, [gun, dispatch, keychain]);
+  }, [dispatch]);
 
   return (
     <div className="App">
@@ -65,7 +78,7 @@ export default function App() {
             const action = await sign({
               op: "add",
               path: "/counter",
-              value: (state.counter || 0) + 1
+              value: (state.counter || 0) + 1,
             });
             // @ts-ignore
             const newOpRef = gun
@@ -73,7 +86,10 @@ export default function App() {
               .get("actions")
               .get(action.operationToken)
               .put(action as never);
-            gun.get(userId).set(newOpRef as never);
+            gun
+              .get("peerstate-example")
+              .get(userId)
+              .set(newOpRef as never);
           } catch (e) {
             console.error(e);
           }
